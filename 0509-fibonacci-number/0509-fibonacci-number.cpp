e3da1cc954818134de43390ee93ddf4f60734a0c
sameer achara <sameerachara894@gmail.com>
2026-08-22 19:26:40 +0530

    Time: 2 ms (77.37%), Space: 7.6 MB (88.86%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int fib(int n) {
+        int c;
+        int a=0;
+        int b=1;
+        for (int i = 1; i <= n; i++) {
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    return a;
+    }
+};
